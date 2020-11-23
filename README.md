ce6cd4d6ff4c7a17703392a8f9fa9cf6080a26c6
Plotzes <plotzes@email.com>
2020-11-23 23:58:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# leaderboard
+ A TNT Wizards leaderboard at https://plotzes.ml/leaderboard
---
+++ assets/javascript.js
@@ -0,0 +1,22 @@
+async function loadLeaderboard() {
+    let lb = await fetch("https://leaderboard.plotzes.workers.dev");
+    lb = await lb.json();
+    if(lb.success) {
+        lbData = lb.lb_data;
+        let tbody = document.querySelector("tbody");
+        const lbLength = lbData.length;
+        for(let i = 0; i < lbLength; i++) {
+            const player = lbData[i];
+            const row = document.createElement("tr");
+            const innerRow = `
+            <td>${i + 1}</td>
+            <td><a href="https://plotzes.ml/stats/?player=${player.name}" target="_blank">${player.name}</a></td>
+            <td>${player.points_capture}</td>
+            <td>${new Date(player.last_update).toLocaleDateString()}</td>`;
+            row.innerHTML = innerRow;
+            tbody.appendChild(row);
+        }
+    }
+}
+
+window.onload = loadLeaderboard;
